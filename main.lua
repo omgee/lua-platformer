51e75c5cde1fb6443da2a35a5a4ece9efce14bb5
omgee <omgee@programmer.net>
2015-05-25 19:14:33 +0000

    Инициализация

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+  
+--  Концигурация окошка и цветов Love2D
+
+  gr = love.graphics
+  wn = love.window
+  kb = love.keyboard
+  gr.setBackgroundColor(255, 255, 255)
+  gr.setColor(0, 0, 0)
+  wn.setMode(320, 320)
+  wn.setTitle("Test platformer")
+  
+--  -----------------------------------
+
+end
